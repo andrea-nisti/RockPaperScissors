85324b4dcab3e0d147931c1cfe8e0248d350bb21
Andrea Nisticò <andrea.nistic@gmail.com>
2018-08-30 17:12:49 +0000

    remove central authority

--- contracts/RockPaperScissors.sol
+++ contracts/RockPaperScissors.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.23;
 
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
-import 'openzeppelin-solidity/contracts/lifecycle/Pausable.sol';
-import 'openzeppelin-solidity/contracts/lifecycle/Destructible.sol';
-import 'openzeppelin-solidity/contracts/math/Math.sol';
+import "node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "node_modules/openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
+import "node_modules/openzeppelin-solidity/contracts/lifecycle/Destructible.sol";
+import "node_modules/openzeppelin-solidity/contracts/math/Math.sol";
 
 /**
  You will create a smart contract named RockPaperScissors whereby:
@@ -33,83 +33,87 @@ contract RockPaperScissors is Ownable, Pausable, Destructible{
     
     struct Game {
 
-        bool doIExist;
-
         address p1;
         address p2;
-
-        bool p1Payed;
-        bool p2Payed;
- 
+        Hand move2;
         uint buyIn;
         bytes32 secretKey1;
-        bytes32 secretKey2;
+
     }
 
     //events
-
-    event LogGameCreated(uint _gameId, uint _buyIn);
-    //event LogPlayerEnrolled(uint _gameId, uint _buyIn);
+    event LogGameCreated(uint gameId, uint buyIn, bytes32 secret, address player1);
+    event LogPlayer2Enrolled(uint gameId, Hand move, address player2);
+    event LogPlayedGame(uint gameId, uint pass1, Hand move1, Hand move2, address winner);
+    event LogNewWithdraw(uint amount, address who);
 
     constructor () public {}    
 
-    function createGame (uint _gameId, uint _buyIn) external onlyOwner returns(bool res)   {
+    //Create a game if it is not there
+    function readyPlayer1 (uint gameId, uint _buyIn, bytes32 secret) external returns(bool res)   {
         
-        require(games[_gameId].doIExist == false,"Existing game");
-        games[_gameId].doIExist = true;
-        emit LogGameCreated(_gameId,_buyIn);
+        require(games[gameId].p1 == address(0),"Existing game");
+        require(balances[msg.sender] >= games[gameId].buyIn, "Not enough balance");
+
+        games[gameId] = Game({
+            p1 : msg.sender,
+            secretKey1 : secret,
+            buyIn : _buyIn
+        });
+        balances[msg.sender] -= _buyIn;
+        
+        emit LogGameCreated(gameId,_buyIn,secret);
         return true;
     }
 
-    function enroll (uint _gameId, bytes32 _secretHand) external returns(bool res)  {
+    //Enroll to the game, 
+    //hand is clear but who cares now -> We could hash the move in the future for hardcore players
+    function readyPlayer2(uint gameId, Hand move2) external returns(bool res)  {
         
-        require(games[_gameId].doIExist,"Game not existing");
-        require(!games[_gameId].p1Payed || !games[_gameId].p2Payed,"Ongoing Game");
-        require(balances[msg.sender] >= games[_gameId].buyIn);
-
-        balances[msg.sender] -= games[_gameId].buyIn;
+        address tPlayer1 = games[gameId].p1;
+        require(tPlayer1 != address(0), "Game not existing");
+        //Now we should have a buy in, save some gas
+        uint tBuyIn = games[gameId].buyIn;
+        require(msg.sender != tPlayer1, "You can't play alone");
+        require(balances[msg.sender] >= tBuyIn, "Not enough balance");
+        require(move2 != Hand.EMPTY, "You cannot play an empty hand, sorry");
         
-        if(!games[_gameId].p1Payed){
-            games[_gameId].p1 = msg.sender;
-            games[_gameId].secretKey1 = _secretHand;
-            games[_gameId].p1Payed = true;
-        }else if(!games[_gameId].p2Payed){
-            games[_gameId].p2 = msg.sender;
-            games[_gameId].secretKey2 = _secretHand;
-            games[_gameId].p2Payed = true;
-        }else {
-            revert();
-        }
+        //Fill missing members
+        games[gameId].p2 = msg.sender;
+        games[gameId].move2 = move2;
+        balances[msg.sender] -= tBuyIn;
+        
+        emit LogPlayer2Enrolled(gameId,move2,msg.sender);
         return true;
-
     }
 
-    function playGame (uint _gameId, uint _pass1, Hand _h1, uint _pass2, Hand _h2) external returns(address winnerAddr, bool res) {
+    //Play the game nd find out who's the lucky winner, Draw is not managed now -> game closes, you lose gas ¯\_( '_')_/¯
+    function playGame (uint gameId, uint pass1, Hand h1) external returns(address winnerAddr, bool res) {
         
-        require(games[_gameId].doIExist == true,"Game not existing");
-        require(games[_gameId].p1Payed && games[_gameId].p2Payed,"Wating for players");
-        require (_h1 != Hand.EMPTY && _h2 != Hand.EMPTY,"You should play");
+        require(games[gameId].p2 == address(0),"Existing game");
+        require(h1 != Hand.EMPTY, "You cannot play an empty hand, sorry");
         
-        bytes32 hash1 = keccak256(abi.encodePacked(_pass1, uint(_h1), games[_gameId].p1));
-        bytes32 hash2 = keccak256(abi.encodePacked(_pass2, uint(_h2), games[_gameId].p2));
-
-        require (hash1 == games[_gameId].secretKey1, "Player one is cheating...");
-        require (hash2 == games[_gameId].secretKey2, "Player two is cheating...");
+        bytes32 hash1 = computeHash(pass1, h1);
+        require (hash1 == games[gameId].secretKey1, "Player one is cheating...");
         
-        uint winner = compare(_h1, _h2);
+        uint tBuyIn = games[gameId].buyIn;
+        Hand h2 = games[gameId].move2;
+        uint winner = compare(h1, h2);
+        
+        //Should I save gas here by saving addresses? It is an if block so it shouldn't matters
         if(winner == 1){
-            balances[games[_gameId].p1] += 2*games[_gameId].buyIn;
-            winnerAddr = games[_gameId].p1;
+            balances[games[gameId].p1] += 2*tBuyIn;
+            winnerAddr = games[gameId].p1;
         }else if (winner == 2){
-            balances[games[_gameId].p1] += 2*games[_gameId].buyIn;
-            winnerAddr = games[_gameId].p2;
+            balances[games[gameId].p1] += 2*tBuyIn;
+            winnerAddr = games[gameId].p2;
         }else{
-            balances[games[_gameId].p1] += games[_gameId].buyIn;
-            balances[games[_gameId].p2] += games[_gameId].buyIn;
-            winnerAddr = address(0);
-            res = false;    
+            balances[games[gameId].p1] += tBuyIn;
+            balances[games[gameId].p2] += tBuyIn;
+            winnerAddr = address(0); 
         }
-        delete games[_gameId];
+        emit LogPlayedGame(gameId, pass1, h1, h2, winnerAddr);
+        delete games[gameId];
         res = true;
 
     }
@@ -117,38 +121,37 @@ contract RockPaperScissors is Ownable, Pausable, Destructible{
     //1-rock ; 2-paper ; 3-scissors
     function compare(Hand move1, Hand move2) internal pure returns (uint win) {
         if (move1 == move2) return 0;
-        if ( (move1 == Hand.ROCK && move2==Hand.PAPER) || 
-             (move1 == Hand.PAPER && move2==Hand.SCISSORS) ||
+        if ( (move1 == Hand.ROCK && move2==Hand.PAPER) ||
+             (move1 == Hand.PAPER && move2==Hand.SCISSORS)||
              (move1 == Hand.SCISSORS && move2==Hand.ROCK) ) return 2;
-        if ( (move1 == Hand.ROCK && move2==Hand.SCISSORS) || 
+        if ( (move1 == Hand.ROCK && move2==Hand.SCISSORS) ||
              (move1 == Hand.PAPER && move2==Hand.ROCK) ||
              (move1 == Hand.SCISSORS && move2==Hand.PAPER) ) return 1;
          
     }
 
     function withdraw (uint amount2wd) external returns(bool res)  {
-       require(amount2wd > 0);
-       require(balances[msg.sender]>0);
-       require(amount2wd <= balances[msg.sender], "Insufficient Balance");       
-       
-       balances[msg.sender] -= amount2wd;
-       msg.sender.transfer(amount2wd);
-       return true;
+        require(balances[msg.sender] > 0, "Empty balance");
+        require(balances[msg.sender] >= amount2wd,"Insufficient Balance");       
 
+        //Let us be optimistic
+        balances[msg.sender] -= amount2wd;
+        emit LogNewWithdraw(amount2wd, msg.sender);
+        msg.sender.transfer(amount2wd);
+        return true;
     }
+
     //Helpers
-    function computeHash(uint ps, uint m, address adr) public pure returns(bytes32){
-        return keccak256(abi.encodePacked(ps,m,adr));
+    function computeHash(uint pswrd, Hand move) public pure returns(bytes32){
+        return keccak256(abi.encodePacked(pswrd,move));
     }
-
+    //Usefull for remix
     function checkBalance (address addr) public view returns(uint bal)  {
         return balances[addr];
     }
     
-
     //Fallback fun
     function () public payable  {
-        require (msg.value > 0 , "You should send some ether");
         balances[msg.sender] += msg.value;
     }
     
